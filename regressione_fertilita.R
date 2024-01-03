e5109d5cf6bd95118d8ac8dec03164146acf67a0
Annalaura Miglino <miglinoannalaura@gmail.com>
2024-01-03 20:21:27 +0100

    Aggiustati commenti

--- regressione_fertilita.R
+++ regressione_fertilita.R
@@ -4,10 +4,11 @@
 #                       OPERAZIONI PRELIMINARI
 #________________________________________________________________________
 
-
+#Si importano le librerie necessarie
 library(readxl)
 library(ggplot2)
 
+#Si legge il set di dati
 data <- read_excel("dataset_puliti/fertilita_arrotondato.xlsx")
 
 # Visualizzo il dataframe
@@ -15,17 +16,16 @@ View(data)
 
 
 #________________________________________________________________________
-#           REGRESSIONE LINEARE SINGOLA - 2021 con 2010
+#             REGRESSIONE LINEARE SINGOLA - 2021 e 2010
 #________________________________________________________________________
 
-
-#Calcolo la covarianza campionaria
+#Si calcola la covarianza campionaria
 cov(data$`2010`, data$`2021`)
 
-#Calcolo il coefficiente di correlazione campionario
+#Si calcola il coefficiente di correlazione campionario
 cor(data$`2010`, data$`2021`)
 
-#Otteniamo lo scatterplot
+#Si crea lo scatterplot
 plot(data$`2010`, data$`2021`, main = "Retta di regressione",
      xlab = "Fertilità nel 2010", ylab = "Fertilità nel 2021",
      col = "red")
@@ -33,17 +33,16 @@ abline(lm(data$`2021` ~ data$`2010`), col = "blue")
 
 
 #________________________________________________________________________
-#           REGRESSIONE LINEARE SINGOLA - 2021 con 2020
+#             REGRESSIONE LINEARE SINGOLA - 2021 e 2020
 #________________________________________________________________________
 
-
-#Calcolo la covarianza campionaria
+#Si calcola la covarianza campionaria
 cov(data$`2020`, data$`2021`)
 
-#Calcolo il coefficiente di correlazione campionario
+#Si calcola il coefficiente di correlazione campionario
 cor(data$`2020`, data$`2021`)
 
-#Otteniamo lo scatterplot
+#Si crea lo scatterplot
 plot(data$`2020`, data$`2021`, main = "Retta di regressione",
      xlab = "Fertilità nel 2020", ylab = "Fertilità nel 2021",
      col = "red")
@@ -54,13 +53,13 @@ abline(lm(data$`2021` ~ data$`2020`), col = "blue")
 #                     REGRESSIONE LINEARE MULTIPLA
 #________________________________________________________________________
 
-#Matrice delle covarianze
+#Si calcola la matrice delle covarianze
 cov(data[, -1])
 
-#Matrice delle correlazioni
+#Si calcola la matrice delle correlazioni
 cor(data[, -1])
 
-#Eseguo la regressione lineare
+#Si esegue la regressione lineare
 #La variabile dipendente è l'anno 2021
 #Le variabili indipendenti sono gli anni dal 2010 al 2020
 model <- lm(data$`2021` ~ data$`2010` + data$`2011` + data$`2012` + 
@@ -68,20 +67,20 @@ model <- lm(data$`2021` ~ data$`2010` + data$`2011` + data$`2012` +
               data$`2017` + data$`2018` + data$`2019` + data$`2020`)
 model
 
-#Calcolo il vettore dei valori stimati
+#Si calcola il vettore dei valori stimati
 stime <- fitted(model)
 
-#Calcolo il vettore dei residui
+#Si calcola il vettore dei residui
 residui <- resid(model)
 
-#Calcolo il vettore dei residui standard
+#Si calcola il vettore dei residui standard
 residuiStandard <- residui/sd(residui)
 
-#Costruisco il grafico dei residui in funzione dei valori stimati
+#Si costruisce il grafico dei residui in funzione dei valori stimati
 plot(stime, residuiStandard, 
      main="Residui standard rispetto ai valori stimati", 
      xlab="Valori stimati", ylab="Residui standard", pch=5, col="red")
 abline(h=0, col="blue", lty=2)
 
-#Coefficiente di determinazione
+#Si calcola il coefficiente di determinazione
 summary(model)$r.square
